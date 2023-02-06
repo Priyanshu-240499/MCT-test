27ded91e70bfa36c239098d761c4d2fe1c468cf8
priyanshu <priyanshoochoudhary@gmail.com>
2023-02-06 06:44:12 -0800

    News-App

--- src/Components/Categories/categories.js
+++ src/Components/Categories/categories.js
@@ -5,7 +5,7 @@ function Categories() {
 const category=useParams().category;
 const [categoryData, setCategoryData] = useState([]);
   useEffect(() => {
-    fetch(`http://newsapi.org/v2/top-headlines?country=in&apiKey=a28c557ae3b348d1a678175c292a3348&category=${category}`)
+    fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=a28c557ae3b348d1a678175c292a3348&category=${category}`)
       .then((res) => res.json())
       .then((data) => setCategoryData(data.articles));
   }, [category]);
